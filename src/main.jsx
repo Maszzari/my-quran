8b5557cedfffc410114e0d9e2a631e8add0ea742
maszz <maszzari2005@gmail.com>
2023-02-27 00:57:51 +0800

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from "./componets/App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<QueryClientProvider client={queryClient}>
 				<App />
 				<ReactQueryDevtools />
 			</QueryClientProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
